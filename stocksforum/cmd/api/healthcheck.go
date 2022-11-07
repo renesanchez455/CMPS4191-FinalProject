aa6355f606d7bacb828e519d03bab3736390dfb0
renesanchez455 <renesanchez455@yahoo.com>
2022-11-06 23:26:22 -0600

    set up server, added healthcheck handler to verify proper functionality

---
+++ stocksforum/cmd/api/healthcheck.go
@@ -0,0 +1,13 @@
+// Filename: cmd/api/healthcheck.go
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "status: avaialble")
+	fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	fmt.Fprintf(w, "version: %s\n", version)
+}
