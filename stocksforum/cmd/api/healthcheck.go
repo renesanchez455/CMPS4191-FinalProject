be8d099fcb023ef88b45f20fb93b5d390b2e58c4
renesanchez455 <renesanchez455@yahoo.com>
2022-11-08 08:20:44 -0600

    created a writeJSON helper method for hadlers to call to write responses

--- stocksforum/cmd/api/healthcheck.go
+++ stocksforum/cmd/api/healthcheck.go
@@ -2,7 +2,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -13,17 +12,12 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"environment": app.config.env,
 		"version":     version,
 	}
-	// Convert our map into a JSON object
-	js, err := json.Marshal(data)
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 		return
 	}
-	// Add a newline to make viewing on the terminal easier
-	js = append(js, '\n')
-	// Specifiy that we will serve our responses using JSON
-	w.Header().Set("Content-Type", "application/json")
-	// Write the []byte slice containing the JSON response body
-	w.Write(js)
+
 }
