9615dd7fb368f423e3630a56e28ff15690082642
renesanchez455 <renesanchez455@yahoo.com>
2022-11-08 02:43:29 -0600

    refactored read id parameter code

--- stocksforum/cmd/api/forums.go
+++ stocksforum/cmd/api/forums.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // createSchoolHandler for the "POST /v1/schools" endpoint
@@ -15,11 +12,8 @@ func (app *application) createForumHandler(w http.ResponseWriter, r *http.Reques
 
 // showSchoolHandler for the "GET /v1/schools/:id" endpoint
 func (app *application) showForumHandler(w http.ResponseWriter, r *http.Request) {
-	// Use the "ParamsFromContext()" function to get the request context as a slice
-	params := httprouter.ParamsFromContext(r.Context())
-	// Get the value of the "id" parameter
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
