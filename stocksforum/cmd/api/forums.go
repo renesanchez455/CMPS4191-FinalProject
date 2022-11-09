77e0d58a160fe44320cb658883f74481aac07ab9
renesanchez455 <renesanchez455@yahoo.com>
2022-11-09 01:59:48 -0600

    Added code that handles bad requests and restricts requests

--- stocksforum/cmd/api/forums.go
+++ stocksforum/cmd/api/forums.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -17,9 +16,9 @@ func (app *application) createForumHandler(w http.ResponseWriter, r *http.Reques
 		Message string `json:"message"`
 	}
 	// Initialize a new json.Decoder instance
-	err := json.NewDecoder(r.Body).Decode(&input)
+	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 	// Display the request
