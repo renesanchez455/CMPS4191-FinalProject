13d1128a7e8674ea0f91c2f24b0b9da38a715686
renesanchez455 <renesanchez455@yahoo.com>
2022-12-02 21:01:09 -0600

    Added CORS middleware

--- stocksforum/cmd/api/middleware.go
+++ stocksforum/cmd/api/middleware.go
@@ -182,3 +182,13 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 
 	return app.requireActivatedUser(fn)
 }
+
+// Enable CORS
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// set the Access-Control-Allow-Origin header
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		next.ServeHTTP(w, r)
+	})
+}
